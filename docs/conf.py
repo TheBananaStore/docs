ec24985533e0208f7230cd456781adf9169ef57d
Alexey "LEHAtupointow" Pavlov <pezleha@gmail.com>
2022-03-29 07:53:47 -0800

    Use custom theme

--- docs/conf.py
+++ docs/conf.py
@@ -4,21 +4,11 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
 
 # -- Project information -----------------------------------------------------
 
 project = 'The Banana Store'
-copyright = '2022, Alexey Pavlov, Grimet'
+copyright = '2022, The banana Team'
 author = 'Alexey Pavlov, Grimet'
 
 # The full version, including alpha/beta/rc tags
@@ -47,7 +37,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
